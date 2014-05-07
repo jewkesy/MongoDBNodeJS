1e8dbafd10a8db960b8bafe80f9a65b2ebe4ac7a
Daryl Jewkes <github@daryljewkes.com>
2014-05-07 12:41:00 +0100

    preparing code for question 7 application

--- final_exam/question_7/importData.sh
+++ final_exam/question_7/importData.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mongoimport -d question7 -c albums --drop < final7/final7/albums.json
 mongoimport -d question7 -c images --drop < final7/final7/images.json
+mongo question7 --eval "db.albums.ensureIndex({'images' : 1})"
+mongo question7 --eval "db.images.ensureIndex({'tags' : 1})"
